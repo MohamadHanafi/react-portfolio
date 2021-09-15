b717051af1eb2bb284189ea0dd31af9cfb25bec8
Mohamad Hanafi <m.hanafi122@gmail.com>
2021-09-15 18:45:51 +0300

    fix some bygs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
           <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
             <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
               {t('about.greeting1')}
-              <br className='hidden lg:inline-block' />
+              <br className='inline-block' />
               {t('about.greeting2')}
             </h1>
             <p className='mb-8 leading-relaxed'>{t('about.summary')}</p>
