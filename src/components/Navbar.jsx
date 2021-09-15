7ce0b0f0470560ae27c069967a026ac3d86989f6
Mohamad Hanafi <m.hanafi122@gmail.com>
2021-09-15 14:40:28 +0300

    adding arabic support

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { ArrowRightIcon } from '@heroicons/react/solid';
+import { useTranslation } from 'react-i18next';
 
 const Navbar = () => {
+  const lngs = {
+    en: { nativeName: 'En' },
+    ar: { nativeName: 'Ar' },
+  };
+
+  const { t, i18n } = useTranslation();
+
   return (
     <header className='bg-gray-800 md:sticky top-0 z-10'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
@@ -10,25 +18,39 @@ const Navbar = () => {
           className='title-font font-medium text-white mb-4 md:mb-0'
         >
           <a href='#about' className='ml-3 text-xl'>
-            Mohamad Hanafi
+            {t('nav.name')}
           </a>
         </a>
         <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center'>
           <a href='#projects' className='mr-5 hover:text-white'>
-            Past Work
+            {t('nav.pastWork')}
           </a>
           <a href='#skills' className='mr-5 hover:text-white'>
-            Skills
+            {t('nav.skills')}
           </a>
           <a href='#certificates' className='mr-5 hover:text-white'>
-            Certificates
+            {t('nav.certificates')}
           </a>
         </nav>
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              key={lng}
+              type='submit'
+              onClick={(e) => {
+                i18n.changeLanguage(lng);
+              }}
+              className='mx-1 font-bold rounded text-lg border-green-500 border-2 px-2 hover:text-white hover:bg-green-500'
+            >
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <a
           href='#contact'
           className='inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0'
         >
-          Hire Me
+          {t('nav.hireMe')}
           <ArrowRightIcon className='w-4 h-4 ml-1' />
         </a>
       </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,21 @@
 import { BadgeCheckIcon, ChipIcon } from '@heroicons/react/solid';
 import React from 'react';
 import { skills } from '../portfolio/skills';
+import { useTranslation } from 'react-i18next';
 
 const Skills = () => {
+  const { t } = useTranslation();
+
   return (
     <section id='skills'>
       <div className='container px-5 py-10 mx-auto '>
         <div className='text-center mb-20'>
           <ChipIcon className='w-10 inline-block mb-4' />
           <h1 className='sm:text-4xl text-3xl font-medium title-font text-white mb-4'>
-            Skills &amp; Technologies
+            {t(`skills.header`)}
           </h1>
           <p className='text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto'>
-            "Life is all about learning", which is why I always try to learn and
-            improve my skills. Here are some of the main skills that I use in my
-            work.
+            {t(`skills.paragraph`)}
           </p>
         </div>
         <div className='flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2'>
